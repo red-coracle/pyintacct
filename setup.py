746a863ac5eb2c32f894d2de568d00bbdc6d138d
red-coracle <red-coracle@users.noreply.github.com>
2019-07-23 21:43:00 -0400

    Increment version for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 NAME = 'pyintacct'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Python SDK for Intacct'
 LONG_DESCRIPTION = open('README.md').read()
 LICENSE = 'MIT'
