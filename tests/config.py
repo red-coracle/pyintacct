335b754a426564dc6b6ac28033f65fc2490d1f0b
red-coracle <red-coracle@users.noreply.github.com>
2019-09-30 14:09:43 -0400

    pytest action (#2)
    Run unit tests automatically

---
+++ tests/config.py
@@ -0,0 +1,8 @@
+import os
+
+
+config = {'SENDER_ID': os.getenv('SENDER_ID'),
+          'SENDER_PW': os.getenv('SENDER_PW'),
+          'USER_ID': os.getenv('USER_ID'),
+          'USER_PW': os.getenv('USER_PW'),
+          'COMPANY_ID': os.getenv('COMPANY_ID')}
